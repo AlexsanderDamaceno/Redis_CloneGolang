6ec7fb82420ead2b65197f093f89a420ea36fe56
Alexsander Damaceno <aemgbo@gmail.com>
2024-09-18 02:35:22 -0300

    setup

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import 
+(
+    "log"
+    "net"
+)
+
+func main() {
+
+    listener, err := net.Listen("tcp", ":1234")
+
+    if err != nil {
+        log.Fatalf("Failed to start server: %v", err)
+    }
+
+    log.Println("Redis server is running on :1234")
+
+    for {
+
+        conn, err := listener.Accept()
+
+        if err != nil {
+            log.Printf("Failed to accept connection: %v", err)
+            continue
+        }
+
+    }
+
+}
